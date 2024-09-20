90a9021776f7e1cf9d13543251af04d519647662
Jared <janthonyborja@gmail.com>
2024-09-20 16:22:25 -0500

    Created equation for putting the airport tower in the center

--- src/visualization/AirportVisuals.java
+++ src/visualization/AirportVisuals.java
@@ -18,23 +18,28 @@ public class AirportVisuals extends JFrame{
 	 * Constructor of class AirportVisuals. Calls the method intializeWindow 
 	 * to create the window within the constructor
 	 * 
-	 * @param width 	Takes in the width of the window
-	 * @param height	Takes in the height of the window
+	 * @param windowWidth 	Takes in the width of the window
+	 * @param windowHeight	Takes in the height of the window
 	 */
-	public AirportVisuals(int width, int height) {
-		intializeWindow(width, height); // Calls the initialize method and creates the Window
+	public AirportVisuals(int windowWidth, int windowHeight) {
+		intializeWindow(windowWidth, windowHeight); // Calls the initialize method and creates the Window
 	}
 	
 	/**
 	 * Method that initializes the creation of the window
 	 * 
-	 * @param width		Takes in the width given by the constructor
-	 * @param height	Takes in the height given by the constructor
+	 * @param windowWidth		Takes in the width given by the constructor
+	 * @param windowHeight	Takes in the height given by the constructor
 	 */
-	public void intializeWindow(int width, int height) {
+	public void intializeWindow(int windowWidth, int windowHeight) {
+		final int towerWidth = 300;
+		final int towerHeight = 300;
+		int centerObjectWidth = (windowWidth - towerWidth) / 2;
+		int cneterObjectHeight= (windowHeight - towerHeight) / 2;
+		
 		setTitle("ATC Simulation"); // Sets the title of the Window to ATC Simulation
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Fully closes the Window
-		setSize(width, height); // Initialize the Height and Width of the window
+		setSize(windowWidth, windowHeight); // Initialize the Height and Width of the window
 		setLocationRelativeTo(null); // Sets the Location of the window to the center of the screen
 		setBackground(Color.BLACK);
 		
@@ -43,7 +48,7 @@ public class AirportVisuals extends JFrame{
 			@Override
 			public void paint(Graphics g) {
 				g.setColor(Color.red);
-				g.fillRect(100, 100, 300, 300);
+				g.fillRect(centerObjectWidth, cneterObjectHeight, towerWidth, towerHeight);
 			}
 		};
 		
