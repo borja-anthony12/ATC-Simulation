6a6c8764e76af7bb8f87554da9da5417ec8197bd
Jared <janthonyborja@gmail.com>
2024-09-20 13:27:10 -0500

    Imported Airport Visuals and created an instance

--- src/visualization/Main.java
+++ src/visualization/Main.java
@@ -1,12 +1,18 @@
 package src.visualization;
 
+// Module imports from src.main
 import src.main.ATC;
 import src.main.Plane;
+
+// Local Module Imports
+import src.visualization.AirportVisuals;
 
 public class Main {
 	public static void main(String[] args) {
 		ATC atc = new ATC();
 		Plane plane = new Plane();
+		AirportVisuals apVisuals = new AirportVisuals();
+		
 		System.out.println("Main Class Works!");
 		
 		plane.test();
