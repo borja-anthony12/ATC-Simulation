c717a3e7f43f19ada7ef8bb6459e7df584f78555
Jared <janthonyborja@gmail.com>
2024-09-20 13:56:19 -0500

    Created an initialize class and created a constructor

--- src/visualization/AirportVisuals.java
+++ src/visualization/AirportVisuals.java
@@ -2,13 +2,36 @@ package src.visualization;
 
 import javax.swing.*;
 
-public class AirportVisuals {
-	JFrame window;
+/**
+ * Class which creates all of the ATC Simulation Visuals
+ */
+public class AirportVisuals extends JFrame{
+	/**
+	 * UID Serial for class
+	 */
+	private static final long serialVersionUID = 2178084625298728326L;
 	
-	public void WindowCreate() {
-		window = new JFrame("Photo Album");
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setSize(600, 600);
-		window.setLocationRelativeTo(null);
+	/**
+	 * Constructor of class AirportVisuals. Calls the method intializeWindow 
+	 * to create the window within the constructor
+	 * 
+	 * @param width 	Takes in the width of the window
+	 * @param height	Takes in the height of the window
+	 */
+	public AirportVisuals(int width, int height) {
+		intializeWindow(width, height); // Calls the initialize method and creates the Window
+	}
+	/**
+	 * Method that initializes the creation of the window
+	 * 
+	 * @param width		Takes in the width given by the constructor
+	 * @param height	Takes in the height given by the constructor
+	 */
+	public void intializeWindow(int width, int height) {
+		setTitle("ATC Simulation"); // Sets the title of the Window to ATC Simulation
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Fully closes the Window
+		setSize(width, height); // Initialize the Height and Width of the window
+		setLocationRelativeTo(null); // Sets the Location of the window to the center of the screen
+		setVisible(true); // Sets window to visible
 	}
 }
