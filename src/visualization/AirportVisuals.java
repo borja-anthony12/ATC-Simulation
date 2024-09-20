9e7d80d3c04745f6a766129aa7a9dea5fdf40220
Jared <janthonyborja@gmail.com>
2024-09-20 17:46:09 -0500

    Created a RunwayPanel class that extends JPanel and add DrawyRunways class

--- src/visualization/AirportVisuals.java
+++ src/visualization/AirportVisuals.java
@@ -1,17 +1,12 @@
 package src.visualization;
 
-import java.awt.Color;
-import java.awt.Graphics;
-
+import java.awt.*;
 import javax.swing.*;
 
 /**
  * Class which creates all of the ATC Simulation Visuals
  */
 public class AirportVisuals extends JFrame{
-	/**
-	 * UID Serial for class
-	 */
 	private static final long serialVersionUID = 2178084625298728326L;
 	
 	/**
@@ -22,38 +17,72 @@ public class AirportVisuals extends JFrame{
 	 * @param windowHeight	Takes in the height of the window
 	 */
 	public AirportVisuals(int windowWidth, int windowHeight) {
-		intializeWindow(windowWidth, windowHeight); // Calls the initialize method and creates the Window
+		intializeWindow(windowWidth, windowHeight); // Calls the initialiseWindow method and creates the Window
 	}
 	
 	/**
-	 * Method that initializes the creation of the window
+	 * Method that initialises the creation of the window
 	 * 
-	 * @param windowWidth		Takes in the width given by the constructor
+	 * @param windowWidth	Takes in the width given by the constructor
 	 * @param windowHeight	Takes in the height given by the constructor
 	 */
-	public void intializeWindow(int windowWidth, int windowHeight) {
-		final int towerWidth = 300;
-		final int towerHeight = 300;
-		int centerObjectWidth = (windowWidth - towerWidth) / 2;
-		int cneterObjectHeight= (windowHeight - towerHeight) / 2;
+	private void intializeWindow(int windowWidth, int windowHeight) {
+		RunwayPanel runway = new RunwayPanel(windowWidth, windowHeight);
 		
 		setTitle("ATC Simulation"); // Sets the title of the Window to ATC Simulation
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Fully closes the Window
-		setSize(windowWidth, windowHeight); // Initialize the Height and Width of the window
-		setLocationRelativeTo(null); // Sets the Location of the window to the center of the screen
-		setBackground(Color.BLACK);
+		setSize(windowWidth, windowHeight); // Initialise the Height and Width of the window
+		setLocationRelativeTo(null); // Sets the Location of the window to the centre of the screen
+		setBackground(Color.black);
 		
-		JPanel Airport = new JPanel() {
-			
-			@Override
-			public void paint(Graphics g) {
-				g.setColor(Color.red);
-				g.fillRect(centerObjectWidth, cneterObjectHeight, towerWidth, towerHeight);
-			}
-		};
 		
-		add(Airport);
+		add(runway);
 		
 		setVisible(true); // Sets window to visible
+	}
+	
+	/**
+	 * Class that extends JPanel and creates runways
+	 */
+	private class RunwayPanel extends JPanel {
+		private int windowWidth;
+		private int windowHeight;
+		
+		/**
+		 * Runway Constructor that takes in 
+		 * the windowWidth and Height which
+		 * is used to calculate 
+		 * the position of the runway
+		 * 
+		 * @param windowWidth	takes in Width of the Window
+		 * @param windowHeight	takes the Height of the Window
+		 */
+		public RunwayPanel(int windowWidth, int windowHeight) {
+			this.windowWidth = windowWidth;
+			this.windowHeight = windowHeight;
+		}
+		
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			drawRunways(g);
+		}
+		
+		/**
+		 * Creates the runway and does the calculations for
+		 * the position of the runway
+		 * 
+		 * @param g takes in the graphics and calls it g
+		 */
+		 public void drawRunways(Graphics g) {
+			 	/* runway width and height*/
+	            final int runwayWidth = 150;
+	            final int runwayHeight = 300;
+	            int runwayX = (windowWidth - runwayWidth) / 2;
+	            int runwayY = (windowHeight - runwayHeight) / 2;
+
+	            g.setColor(Color.GRAY);  // Set runway colour
+	            g.fillRect(runwayX, runwayY, runwayWidth, runwayHeight);  // Draw the runway
+	        }
 	}
 }
