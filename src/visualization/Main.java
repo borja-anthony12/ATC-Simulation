c1409fc6a6cd9824babfc493ed27e1065c270343
Lim Ping <janthonyborja@gmail.com>
2024-09-25 11:58:40 -0500

    Changed variable names to proper naming conventions

--- src/visualization/Main.java
+++ src/visualization/Main.java
@@ -9,13 +9,13 @@ import src.visualization.AirportVisuals;
 
 public class Main {
 	/* Width and Height */
-	private static final int width = 720; 	// Width for window
-	private static final int height = 600;	// Height for window
+	private static final int WIDTH = 720; 	// Width for window
+	private static final int HEIGHT = 600;	// Height for window
 	
 	public static void main(String[] args) {
 		// Creates instances of all of the imported modules
 		Tower tower = new Tower();
 		PlaneAttributes planeAttributes = new PlaneAttributes();
-		AirportVisuals apVisuals = new AirportVisuals(width, height);
+		AirportVisuals apVisuals = new AirportVisuals(WIDTH, HEIGHT);
 	}
 }
