e94610d895205200d28905aee20e7b511fee4a71
MeatballsCalls <klmnkehler@gmail.com>
2024-09-26 12:06:24 -0500

    added changes

--- src/main/Tower.java
+++ src/main/Tower.java
@@ -8,65 +8,68 @@ import java.util.Arrays;
  */
 public class Tower {
 	
-	protected ArrayList<PlaneAttributes> planeAttributes;
+	protected ArrayList<PlaneAttributes> planes;
 	private PlaneAttributes[] gatePlanes;
 	private boolean[] gates;
 	private int minimumSpaceBetweenPlanes;
 	
+	/**
+	 * 
+	 * @param gateCount is the number of gates in the airport
+	 */
 	public Tower(int gateCount) {
-		planeAttributes = new ArrayList<PlaneAttributes>();
+		planes = new ArrayList<PlaneAttributes>();
 		gates = new boolean[gateCount];
 		gatePlanes = new PlaneAttributes[gateCount];
 	}
 	
-	/*
-	 * Gets list planes for info stuff
-	 */
-	public ArrayList<PlaneAttributes> getPlanes() {
-		return planeAttributes;
-	}
-	
-	/*
+	/**
 	 * Checks if planes collide
 	 */
 	public void checkPlaneCollision() {
-		PlaneAttributes[] planeArray = (PlaneAttributes[]) planeAttributes.toArray();
+		PlaneAttributes[] planeArray = (PlaneAttributes[]) planes.toArray();
 		
 		for(int i = 0; i < planeArray.length; i++) {
 			for(int j = 0; j < planeArray.length; j++) {
 				int dif = comparePlanePos(planeArray[i], planeArray[j]);
+				
 				if(dif < minimumSpaceBetweenPlanes) {
-					planeArray[i].crash();
-					planeArray[j].crash();
+					planeArray[i].isCrashed();
+					planeArray[j].isCrashed();
 				}
 			}
 		}
 	}
 	
-	/*
+	/**
 	 * moves a plane to a gate from the runway
 	 */
 	public void taxiPlaneToGate(PlaneAttributes plane) {
 		for(int i = 0; i < gates.length; i++) {
 			if(!gates[i]) {
 				gates[i] = true;
-				gatePlanes[i] = plane.getCallSign();
+				gatePlanes[i] = plane;
 			}
 		}
 	}
 	
-	/*
+	/**
 	 * moves a plane to the runway from a gate
 	 */
 	public void taxiPlaneToRunway(PlaneAttributes plane) {
 		for(int i = 0; i < gates.length; i++) {
-			if(plane.getCallSign() == gate) {
-				
+			if(plane.getCallSign() == gatePlanes[i].getCallSign()) {
+				gates[i] = false;
 			}
 		}
 	}
 	
-	/*
+	public void spawnPlane() {
+		PlaneAttributes plane = new PlaneAttributes();
+		planes.add(plane);
+	}
+	
+	/**
 	 * gets the difference the X Y values of two planes
 	 * Receives positions in X Y array format
 	 */
@@ -75,5 +78,12 @@ public class Tower {
 		int deltaY = p1.getPosition()[1] - p2.getPosition()[1];
 		int dif = (int) Math.sqrt(deltaX^2 + deltaY^2);
 		return dif;
+	}
+	
+	/**
+	 * Gets list planes for info stuff
+	 */
+	public ArrayList<PlaneAttributes> getPlanes() {
+		return planes;
 	}
 } 
