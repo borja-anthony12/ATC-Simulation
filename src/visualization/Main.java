77015238809463235eeef7dfe197189c9a3589f7
Lim Ping <janthonyborja@gmail.com>
2024-09-26 13:45:35 -0500

    Added Variable for changing the amount of runways

--- src/visualization/Main.java
+++ src/visualization/Main.java
@@ -12,10 +12,12 @@ public class Main {
 	private static final int WIDTH = 720; 	// Width for window
 	private static final int HEIGHT = 600;	// Height for window
 	
+	private static final int RUNWAY_AMOUNT = 3;
+	
 	public static void main(String[] args) {
 		// Creates instances of all of the imported modules
 		Tower tower = new Tower(3);
-		PlaneAttributes planeAttributes = new PlaneAttributes();
-		AirportVisuals apVisuals = new AirportVisuals(WIDTH, HEIGHT);
+//		PlaneAttributes planeAttributes = new PlaneAttributes();
+		AirportVisuals apVisuals = new AirportVisuals(WIDTH, HEIGHT, RUNWAY_AMOUNT);
 	}
 }
