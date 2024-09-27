73ea3b2997cc4e116a2ece7f1638d1ad5ae09db6
Lim Ping <janthonyborja@gmail.com>
2024-09-27 17:57:14 -0500

    Created WindowUpdate class which handles panel updates
    Constructor handles the updating and repaints the Panel for how ever
    often the update interval variable is set to

---
+++ src/visualization/WindowUpdate.java
@@ -0,0 +1,29 @@
+package src.visualization;
+
+import src.main.*;
+import src.visualization.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class WindowUpdate extends JFrame{
+	private static final long serialVersionUID = 1435203151832523191L;
+	
+	/**
+	 * Updates the screen constantly
+	 * 
+	 * @param Panel Takes in the Panel for updating what's on the screen
+	 */
+	public WindowUpdate(JPanel Panel) {
+		Timer timer = new Timer(Main.UPDATE_INTERAVAL,  new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Panel.repaint();
+			}
+		});
+		timer.start();
+	}
+}
