6ed9f5eeabd3c978146f9c49a52be7562566a2ec
Lim Ping <janthonyborja@gmail.com>
2024-09-30 09:05:42 -0500

    Added comments for explaining the code

--- src/visualization/AirportVisuals.java
+++ src/visualization/AirportVisuals.java
@@ -71,137 +71,185 @@ public class AirportVisuals extends JFrame {
     private class AirportPanel extends JPanel {
         private static final long serialVersionUID = 2853523647566452733L;
         
-        private int windowWidth, windowHeight;
-        private int runwayXPos, runwayYPos;
-        private int buildingXPos, buildingYPos;
-        private int gateXPos, gateYPos;
+        /* Initialises Airports X & Y positions */
+        private int runwayXPos, runwayYPos;		// Initialises the runways X and Y
+        private int buildingXPos, buildingYPos;	// Initialises the buildings X and Y
+        private int gateXPos, gateYPos;			// Initialises the gates X and Y
         
-        private final int RUNWAY_WIDTH = 40;
-        private final int RUNWAY_HEIGHT = 450;
-        private final int ROTATION = 60;
-
-        private final int GATE_WIDTH = 25;
-        private final int GATE_HEIGHT = 50;
+        /* Initialises Airport and Window Components X and Y */
+        private int WINDOW_WIDTH, WINDOW_HEIGHT; 	// Initialises the windows width and height
         
-        private final int BUILDING_WIDTH = 325;
-        private final int BUILDING_HEIGHT = 100;
+        private final int RUNWAY_WIDTH = 40;	// Initialises the runway width
+        private final int RUNWAY_HEIGHT = 450;	// Initialises the runway height
+        private final int RUNWAY_ROTATION = 60;	// Initialises the runway rotation
 
+        private final int GATE_WIDTH = 25;	// Initialises the gate width
+        private final int GATE_HEIGHT = 50; // Initialises the gate height
+        
+        private final int BUILDING_WIDTH = 325;		// Initialises the building width
+        private final int BUILDING_HEIGHT = 100;	// Initialises the building height
+
+        /**
+         * Constructor of Airport Panel and initialises the background as black
+         * 
+         * @param windowWidth	Takes in window Width
+         * @param windowHeight	Takes in window Height
+         */
         public AirportPanel(int windowWidth, int windowHeight) {
             setBackground(Color.BLACK);
-            this.windowWidth = windowWidth;
-            this.windowHeight = windowHeight;
+            this.WINDOW_WIDTH = windowWidth;
+            this.WINDOW_HEIGHT = windowHeight;
         }
 
         @Override
         protected void paintComponent(Graphics g) {
-        	/* TO-DO */
             super.paintComponent(g);
-            drawRunways(g, ROTATION, 0, 0); // Creates the first runway
-
+            
+            // Draws runway
+            drawRunways(g, RUNWAY_ROTATION, 0, 0); // Creates the first runway
+            
+            // Checks the runway amount and makes sure that it's not greater than two
             if (RUNWAY_AMOUNT > 1 && RUNWAY_AMOUNT <= 2) {
-                drawRunways(g, -ROTATION, 23, -50); // Creates the second runway
+                drawRunways(g, -RUNWAY_ROTATION, 23, -50); // Creates the second runway
             }
             
-            drawTaxiWays(g);
             
-            drawAirportBuilding(g);
+            drawTaxiWays(g); // Draws taxi way (Currently only draws one but will change once the entirety of the airport is complete)
             
-            drawGates(g);
+            drawAirportBuilding(g);	// Draws the main building of the airport
+            
+            drawGates(g);	// Draws the gates
         }
         
+        /**
+         * Updates the Airport Components whenever the window is size is changed
+         * 
+         * @param newWidth		Takes in the windows new width
+         * @param newHeight		Takes in the windows new height
+         */
         public void updateRunwayPosition(int newWidth, int newHeight) {
-            windowWidth = newWidth;
-            windowHeight = newHeight;
+        	/* Updates the window size */
+            WINDOW_WIDTH = newWidth;		// Updates the original width to new width
+            WINDOW_HEIGHT = newHeight;	// Updates the original height to new height
             
-            // Example to update runway positions dynamically
-            runwayXPos = (windowWidth - RUNWAY_WIDTH) / 2;  // Adjust X position to centre runway
-            runwayYPos = (windowHeight - 250) / 2;  // Adjust Y position to centre runway
-            
-            buildingXPos = (windowWidth - BUILDING_WIDTH) / 2;
-            buildingYPos = (windowHeight - BUILDING_HEIGHT) / 2;
-            
-            gateXPos = (windowWidth - GATE_WIDTH) / 2;
-            gateYPos = (windowHeight - GATE_HEIGHT) / 2;
-        }
-
-        public void drawRunways(Graphics g, int ROTATION, int xPos, int yPos) {
-            Graphics2D g2d = (Graphics2D) g;
-
-            runwayXPos = (windowWidth - (RUNWAY_WIDTH)) / 2 + xPos;
-            runwayYPos = (windowHeight - RUNWAY_HEIGHT) / 2 + yPos;
-            int centreXRunway = runwayXPos + (RUNWAY_WIDTH / 2);
-            int centreYRunway = runwayYPos + (RUNWAY_HEIGHT / 2);
-
-            g2d.setColor(Color.DARK_GRAY);
-            g2d.translate(centreXRunway, centreYRunway);
-            g2d.rotate(Math.toRadians(ROTATION));
-            g2d.translate(-centreXRunway, -centreYRunway);
-
-            Rectangle runwayVisual = new Rectangle(runwayXPos, runwayYPos, RUNWAY_WIDTH, RUNWAY_HEIGHT);
-
-            g2d.draw(runwayVisual);
-            g2d.fill(runwayVisual);
-
-            // Reset canvas transformation
-            g2d.setTransform(g2d.getDeviceConfiguration().getDefaultTransform());
+            /* Updates Airport Components positions dynamically */
+            // Runways
+            runwayXPos = (WINDOW_WIDTH - RUNWAY_WIDTH) / 2;  // Adjust X position to centre runway
+            runwayYPos = (WINDOW_HEIGHT - 250) / 2;  		// Adjust Y position to centre runway
+            // Building
+            buildingXPos = (WINDOW_WIDTH - BUILDING_WIDTH) / 2; 		// Adjusts the X position to centre the building
+            buildingYPos = (WINDOW_HEIGHT - BUILDING_HEIGHT) / 2;	 // Adjusts the Y Position to centre the building
+            // Gate
+            gateXPos = (WINDOW_WIDTH - GATE_WIDTH) / 2;		// Adjusts the X position to centre the building
+            gateYPos = (WINDOW_HEIGHT - GATE_HEIGHT) / 2;	// Adjusts the Y position to centre the building
         }
         
+        /**
+         * Draws runways and calculates the position/centres the runways
+         * 
+         * @param g				Takes in the graphic object
+         * @param rotation		Takes in variable for rotation
+         * @param xPos			Takes in the offset X position
+         * @param yPos			Takes in the offset Y position
+         */
+        public void drawRunways(Graphics g, int rotation, int xPos, int yPos) {
+            Graphics2D g2d = (Graphics2D) g; // Creates an instance of 2d graphics
+            
+            /* Calculates runway X & Y position */
+            runwayXPos = (WINDOW_WIDTH - (RUNWAY_WIDTH)) / 2 + xPos; // Calculates the runway's X position on the X axis and offsets by xPos
+            runwayYPos = (WINDOW_HEIGHT - RUNWAY_HEIGHT) / 2 + yPos;	// Calculates the runway's Y position on the Y axis and offsets by yPos
+            
+            /* Calculates the runway's position when centred */
+            int centreXRunway = runwayXPos + (RUNWAY_WIDTH / 2);	// Centres the runway on the X axis
+            int centreYRunway = runwayYPos + (RUNWAY_HEIGHT / 2);	// Centres the runway on the Y axis
+
+            g2d.setColor(Color.DARK_GRAY); // Sets the runway colour to dark gray
+            g2d.translate(centreXRunway, centreYRunway);	// Translates the runway to the centre
+            g2d.rotate(Math.toRadians(rotation));			// Converts rotation to radians and rotates the runway
+            g2d.translate(-centreXRunway, -centreYRunway);	// Translates the runway back to the centre (This was added due to the runway being in a random position)
+
+            Rectangle runwayVisual = new Rectangle(runwayXPos, runwayYPos, RUNWAY_WIDTH, RUNWAY_HEIGHT); // Creates the runway and sets the X, Y, runway width and height
+
+            g2d.draw(runwayVisual); // Draws the runway on the panel
+            g2d.fill(runwayVisual);	// Fills the runway on the panel
+
+            // Resets any canvas transformation
+            g2d.setTransform(g2d.getDeviceConfiguration().getDefaultTransform()); // Added due to when building, gates and taxi way weren't in predicted position
+        }
+        
+        /**
+         * Creates the airport building and calculates the position
+         * 
+         * @param g	Takes in graphics object
+         */
         public void drawAirportBuilding(Graphics g) {
-        	Graphics2D g2d = (Graphics2D) g;
+        	Graphics2D g2d = (Graphics2D) g; // Creates an instance of 2d graphics
         	
-        	final int offsetY = 100;
-        	buildingXPos = (windowWidth - BUILDING_WIDTH) / 2;
-        	buildingYPos = (windowHeight - (BUILDING_HEIGHT)) / 2 + offsetY;
-        	int centreXBuilding = buildingXPos + (350 / 2);
-        	int centreYBuilding = buildingYPos + (115 / 2);
+        	final int offsetY = 100; // Sets the offset of the building
         	
-        	g2d.setColor(Color.LIGHT_GRAY);
-        	g2d.translate(centreXBuilding, centreYBuilding);
-        	g2d.rotate(Math.toRadians(-30));
-        	g2d.translate(-centreXBuilding, -centreYBuilding);
+        	/* Calculates the buildings X & Y position */
+        	buildingXPos = (WINDOW_WIDTH - BUILDING_WIDTH) / 2;					// Calculates the buildings X position on the X axis
+        	buildingYPos = (WINDOW_HEIGHT - (BUILDING_HEIGHT)) / 2 + offsetY;	// Calculates the buildings Y position on the Y axis and offsets by 100
         	
-        	Rectangle Building = new Rectangle (buildingXPos, buildingYPos, BUILDING_WIDTH, BUILDING_HEIGHT);
+        	/* Calculates the buildings position when centred */
+        	int centreXBuilding = buildingXPos + (350 / 2);	// Calculates the building centre. (350 is what got to the position I wanted)
+        	int centreYBuilding = buildingYPos + (115 / 2); // Calculates the building centre. (115 is what got the the position on I wanted)
         	
-        	g2d.draw(Building);
-        	g2d.fill(Building);
+        	g2d.setColor(Color.LIGHT_GRAY); // Sets the building colour to light gray
+        	g2d.translate(centreXBuilding, centreYBuilding); 	// Translates the building to the centre
+        	g2d.rotate(Math.toRadians(-30));					// Takes in -30 degrees and converts to radians and rotates building
+        	g2d.translate(-centreXBuilding, -centreYBuilding);	// Translates the building back to the centre
         	
-        	g2d.setTransform(g2d.getDeviceConfiguration().getDefaultTransform());
+        	Rectangle Building = new Rectangle (buildingXPos, buildingYPos, BUILDING_WIDTH, BUILDING_HEIGHT); // Creates the building and sets the X, Y, building width and height
+        	
+        	g2d.draw(Building);	// Draws the building on the panel
+        	g2d.fill(Building); // Fills the building on the panel
+        	
+        	
+        	g2d.setTransform(g2d.getDeviceConfiguration().getDefaultTransform()); // Resets any canvas transformation
         }
         
+        /**
+         * Creates the gates and calculates the positions
+         * 
+         * @param g
+         */
         public void drawGates(Graphics g) {
-        	Graphics2D g2d = (Graphics2D) g;
+        	Graphics2D g2d = (Graphics2D) g; // Creates an instance of 2d graphics
 
 
-            final int gateOffsetY = -44;	// Offset to position gates relative to the building (e.g., above the building)
-            final int gateOffsetX = 5;
-            final int gateSpacing = 85;  	// Space between each gate
+            final int gateOffsetY = -44;	// Offsets the gate on the Y axis
+            final int gateOffsetX = 5;		// Offsets the gate on the X axis
+            final int gateSpacing = 85;  	// Determines the spacing between each gate
 
-            // Apply the rotation first for both building and gates together
-            int centerXBuilding = buildingXPos + (BUILDING_WIDTH / 2);
-            int centerYBuilding = buildingYPos + (BUILDING_HEIGHT / 2);
+            /* Calculates the Gates centre */
+            int centerXBuilding = buildingXPos + (BUILDING_WIDTH / 2);		// Calculates the buildings X position on the X axis
+            int centerYBuilding = buildingYPos + (BUILDING_HEIGHT / 2);		// Calculates the buildings Y position on the Y axis
             
-            // Rotate the entire coordinate system around the building's centre
-            g2d.translate(centerXBuilding, centerYBuilding);
-            g2d.rotate(Math.toRadians(-30));
-            g2d.translate(-centerXBuilding, -centerYBuilding);
+            g2d.translate(centerXBuilding, centerYBuilding);	// Translates the gate to the centre
+            g2d.rotate(Math.toRadians(-30));					// Converts -30 to degrees and converts to radians and rotates building
+            g2d.translate(-centerXBuilding, -centerYBuilding);	// Translates the gate back to the centre
 
             // Loop to create multiple gates evenly spaced along the building
             for (int i = 0; i < GATE_AMOUNT; i++) {
-                // Calculate gate position based on the building position and size
-                gateXPos = buildingXPos + (i * gateSpacing) - gateOffsetX;  // Evenly space gates along the building
+                // Calculates gate position based on the building position and size
+                gateXPos = buildingXPos + (i * gateSpacing) - gateOffsetX;  // Evenly spaces gates along the building
                 gateYPos = buildingYPos + gateOffsetY;  // Position gates relative to the building
-
-                // Draw gate
-                g2d.setColor(Color.LIGHT_GRAY);
-                Rectangle Gate = new Rectangle(gateXPos, gateYPos, GATE_WIDTH, GATE_HEIGHT);
-                g2d.draw(Gate);
-                g2d.fill(Gate);
+                
+                g2d.setColor(Color.LIGHT_GRAY); // Sets the gates colour to light gray
+                Rectangle Gate = new Rectangle(gateXPos, gateYPos, GATE_WIDTH, GATE_HEIGHT); // Creates the gate and sets the X, Y, gate width and height
+                g2d.draw(Gate);	// Draws gate
+                g2d.fill(Gate); // Fills gate
             }
 
-            // Reset transform
-            g2d.setTransform(g2d.getDeviceConfiguration().getDefaultTransform());
+            g2d.setTransform(g2d.getDeviceConfiguration().getDefaultTransform()); // Resets any canvas transformation
         }
         
+        /**
+         * Draws and calculates the taxiway's position
+         * 
+         * @param g
+         */
         public void drawTaxiWays(Graphics g) {
         	/* TO-DO */
             Graphics2D g2d = (Graphics2D) g;
