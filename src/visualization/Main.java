32533fbe0d7c8e56006f9b7f66cb6fed4a96f5d3
Lim Ping <janthonyborja@gmail.com>
2024-09-30 08:10:26 -0500

    Created variable for amount of gates

--- src/visualization/Main.java
+++ src/visualization/Main.java
@@ -12,6 +12,7 @@ public class Main {
 	private static final int HEIGHT = 600;	// Height for window
 	
 	private static final int RUNWAY_AMOUNT = 2; // Maximum amount of runways is 2
+	private static final int GATE_AMOUNT = 4; // Maximum amount of gates is 4
 	public static final int UPDATE_INTERAVAL = 1;
 	
 	public static void main(String[] args) {
