85677220565f67a1cd844afa91fef6e6aa36c7a5
Lim Ping <janthonyborja@gmail.com>
2024-10-02 06:53:21 -0500

    Changed something

--- src/visualization/Main.java
+++ src/visualization/Main.java
@@ -11,14 +11,14 @@ public class Main {
 	private static final int WIDTH = 720; 	// Width for window
 	private static final int HEIGHT = 600;	// Height for window
 	
-	private static final int RUNWAY_AMOUNT = 1; // Maximum amount of runways is 2
-	private static final int GATE_AMOUNT = 4; // Maximum amount of gates is 4
+	private static int runwayAmount = 1; // Maximum amount of runways is 2
+	private static int gateAmount = 4; // Maximum amount of gates is 4
 	public static final int UPDATE_INTERAVAL = 1;
 	
 	public static void main(String[] args) {
 		// Creates instances of all of the imported modules
 		Tower tower = new Tower(3);
 //		PlaneAttributes planeAttributes = new PlaneAttributes();
-		GUIElements apVisuals = new GUIElements(WIDTH, HEIGHT, RUNWAY_AMOUNT, GATE_AMOUNT);
+		GUIElements apVisuals = new GUIElements(WIDTH, HEIGHT, runwayAmount, gateAmount);
 	}
 }
