40398e1a26b7ba389a46dbf7d737fc70e9a7d789
Lim Ping <janthonyborja@gmail.com>
2024-10-10 19:00:41 -0500

    Refactored this one not updateWindow

--- src/visualization/GUIElements.java
+++ src/visualization/GUIElements.java
@@ -7,12 +7,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.net.URL;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
@@ -68,7 +64,6 @@ public class GUIElements extends JFrame {
 		this.gateAmount = gateAmount; // Sets the gateAmount to gateAmount
 
 		tower = new Tower(); // Creates an instance of tower class
-		Random rand = new Random();
 
 		window(windowWidth, windowHeight); // Calls the initializeWindow method and creates a window setting the width
 											// and height to windowWidth and windowHeight
@@ -232,21 +227,15 @@ public class GUIElements extends JFrame {
 	private class AirportPanel extends JPanel {
 		private static final long serialVersionUID = 2853523647566452733L;
 
-		private String[] imageLocation = { "/plane-images/plane-blue.png", "/plane-images/plane-jared.png",
-				"/plane-images/plane-cyan.png", "/plane-images/plane-green.png", "/plane-images/plane-purple.png",
-				"/plane-images/plane-nolan.png", "/plane-images/plane-white.png", "/plane-images/plane-will.png" };
-		
 		private final Color taxiWayColour = Color.DARK_GRAY;
 		private final Color runwayColour = Color.GRAY;
 		private final Color buildingColour = Color.LIGHT_GRAY;
+		private final Color background = Color.BLACK;
 
 		/* Initialises Airports X & Y positions */
 		private int runwayXPos, runwayYPos; // Initialises the runways X and Y
 		private int buildingXPos, buildingYPos; // Initialises the buildings X and Y
 		private int gateXPos, gateYPos; // Initialises the gates X and Y
-
-		/* Initialises Airport and Window Components X and Y */
-		private int windowWidth, windowHeight; // Initialises the windows width (720) and height (600)
 
 		private final int RUNWAY_WIDTH = 40; // Initialises the runway width
 		private final int RUNWAY_HEIGHT = 450; // Initialises the runway height
@@ -261,6 +250,15 @@ public class GUIElements extends JFrame {
 
 		private final int TAXIWAY_WIDTH = RUNWAY_WIDTH / 2;
 
+		private final String[] imageLocation = { "/plane-images/plane-blue.png", "/plane-images/plane-jared.png",
+				"/plane-images/plane-cyan.png", "/plane-images/plane-green.png", "/plane-images/plane-purple.png",
+				"/plane-images/plane-nolan.png", "/plane-images/plane-white.png", "/plane-images/plane-will.png" };
+
+		private Graphics2D g2d;
+
+		/* Initialises Airport and Window Components X and Y */
+		private int windowWidth, windowHeight; // Initialises the windows width (720) and height (600)
+
 		/**
 		 * Constructor of Airport Panel and initialises the background as black
 		 * 
@@ -268,7 +266,7 @@ public class GUIElements extends JFrame {
 		 * @param windowHeight Takes in window Height
 		 */
 		public AirportPanel(int windowWidth, int windowHeight) {
-			setBackground(Color.BLACK); // Sets the panel background to black
+			setBackground(background); // Sets the panel background to black
 
 			this.windowWidth = windowWidth; // Sets the windowWidth to windowWidth
 			this.windowHeight = windowHeight; // Sets the windowHeight to windowHeight
@@ -280,49 +278,30 @@ public class GUIElements extends JFrame {
 		@Override
 		protected void paintComponent(Graphics g) {
 			super.paintComponent(g);
+			g2d = (Graphics2D) g;
 
-			drawTaxiWays(g, ((windowWidth - RUNWAY_WIDTH) / 2) - 85, ((windowHeight - RUNWAY_HEIGHT) / 2) - 160,
-					TAXIWAY_WIDTH, RUNWAY_HEIGHT, RUNWAY_ROTATION); // Draws parallel taxi way above
-																						// the
-																						// main runway
+			int taxiWayXPos = taxiWayXPos(windowWidth, RUNWAY_WIDTH);
+			int taxiWayYPos = taxiWayYPos(windowHeight, RUNWAY_HEIGHT);
 
-			drawTaxiWays(g, ((windowWidth - RUNWAY_WIDTH) / 2) - 15, ((windowHeight - RUNWAY_HEIGHT) / 2) - 40,
-					TAXIWAY_WIDTH, RUNWAY_HEIGHT, RUNWAY_ROTATION); // Draws parallel taxi way below
-																						// the
-																						// main runway
+			int[][] taxiWayData = {
+					{ taxiWayXPos - 85, taxiWayYPos - 160, TAXIWAY_WIDTH, RUNWAY_HEIGHT, RUNWAY_ROTATION },
+					{ taxiWayXPos - 15, taxiWayYPos - 40, TAXIWAY_WIDTH, RUNWAY_HEIGHT, RUNWAY_ROTATION },
+					{ taxiWayXPos - 237, taxiWayYPos + 156, TAXIWAY_WIDTH, 155, -30 },
+					{ taxiWayXPos - 190, taxiWayYPos + 126, TAXIWAY_WIDTH, 155, -30 },
+					{ taxiWayXPos - 144, taxiWayYPos + 100, TAXIWAY_WIDTH, 155, -30 },
+					{ taxiWayXPos(windowWidth, BUILDING_WIDTH) - 30, taxiWayYPos(windowHeight, BUILDING_HEIGHT) + 30,
+							BUILDING_WIDTH, 155, -30 } };
 
-			drawTaxiWays(g, ((windowWidth - RUNWAY_WIDTH) / 2) - 237, ((windowHeight - RUNWAY_HEIGHT) / 2) + 156,
-					TAXIWAY_WIDTH, 155, -30);
+			drawALLTaxiWays(taxiWayData);
 
-			drawTaxiWays(g, ((windowWidth - RUNWAY_WIDTH) / 2) - 190, ((windowHeight - RUNWAY_HEIGHT) / 2) + 126,
-					TAXIWAY_WIDTH, 155, -30);
+			checkIfRunway(runwayAmount, taxiWayXPos, taxiWayYPos);
 
-			drawTaxiWays(g, ((windowWidth - RUNWAY_WIDTH) / 2) - 144, ((windowHeight - RUNWAY_HEIGHT) / 2) + 100,
-					TAXIWAY_WIDTH, 155, -30);
+			drawAirportBuilding(); // Draws the main building of the airport
 
-			drawTaxiWays(g, (windowWidth - BUILDING_WIDTH) / 2 - 30, (windowHeight - BUILDING_HEIGHT) / 2 + 30,
-					BUILDING_WIDTH, 155, -30);
-
-			// Checks the runway amount and makes sure that it's not greater than two
-			if (runwayAmount > 1 && runwayAmount <= 2) {
-				drawTaxiWays(g, ((windowWidth - RUNWAY_WIDTH) / 2) + 170, ((windowHeight - RUNWAY_HEIGHT) / 2) - 50,
-						TAXIWAY_WIDTH, 260, 150);
-				drawTaxiWays(g, ((windowWidth - RUNWAY_WIDTH) / 2) - 144, ((windowHeight - RUNWAY_HEIGHT) / 2) - 25,
-						TAXIWAY_WIDTH, 155, 30);
-				drawRunways(g, RUNWAY_ROTATION, -60, -100); // Creates the first runway
-				drawRunways(g, -RUNWAY_ROTATION, 40, -155); // Creates the second runway
-			} else { // If RUNWAY_AMOUNT is less than 1 it draws the taxiway and than runway
-				drawTaxiWays(g, ((windowWidth - RUNWAY_WIDTH) / 2) + 138, ((windowHeight - RUNWAY_HEIGHT) / 2) - 62,
-						TAXIWAY_WIDTH, 159, 150);
-				drawRunways(g, RUNWAY_ROTATION, -60, -100); // Creates the first runway
-			}
-
-			drawAirportBuilding(g); // Draws the main building of the airport
-
-			drawGates(g);
+			drawGates();
 
 			try {
-				drawPlanes(g);
+				drawPlanes();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -352,38 +331,136 @@ public class GUIElements extends JFrame {
 
 		}
 
+		/**
+		 * Checks what the selected amount of runways are available
+		 * 
+		 * @param runwayAmount takes in the amount of runways
+		 * @param taxiWayXPos  takes in taxiWayXPos
+		 * @param taxiWayYPos  takes in taxiWayYPos
+		 */
+		private void checkIfRunway(int runwayAmount, int taxiWayXPos, int taxiWayYPos) {
+			int[][] taxiWayData = { { taxiWayXPos + 170, taxiWayYPos - 50, TAXIWAY_WIDTH, 260, 150 },
+					{ taxiWayXPos - 144, taxiWayYPos - 25, TAXIWAY_WIDTH, 155, 30 }, };
+			
+			int[][] runwayData = { { RUNWAY_ROTATION, -60, -100 }, { -RUNWAY_ROTATION, 40, -155 } };
+
+			// Checks the runway amount and makes sure that it's not greater than two
+			if (runwayAmount > 1 && runwayAmount <= 2) {
+				drawALLTaxiWays(taxiWayData);
+
+				darwALLRunways(runwayData);
+			} else { // If RUNWAY_AMOUNT is less than 1 it draws the taxi way and than runway
+				drawTaxiWays(taxiWayXPos + 138, taxiWayYPos - 62, TAXIWAY_WIDTH, 159, 150);
+
+				drawRunways(RUNWAY_ROTATION, -60, -100); // Creates the first runway
+			}
+		}
+		
+		/**
+		 * Creates all of the taxi ways 
+		 * 
+		 * @param taxiWayData takes in the data of taxi ways
+		 */
+		private void drawALLTaxiWays(int[][] taxiWayData) {
+			for (int[] data : taxiWayData) {
+				drawTaxiWays(data[0], data[1], data[2], data[3], data[4]);
+			}
+		}
+		
+		/**
+		 * Creates all of the runways
+		 * 
+		 * @param runwayData takes in runway data
+		 */
+		private void darwALLRunways(int[][] runwayData) {
+			for (int[] data : runwayData) {
+				drawRunways(data[0], data[1], data[2]);
+			}
+		}
+
+		/**
+		 * Calculates the centred X position relative to an object
+		 * 
+		 * @param windowWidth takes in the window width
+		 * @param objectWidth takes in the object width (i.e., building width and runway
+		 *                    width)
+		 * @return returns calculated X value
+		 */
+		private int taxiWayXPos(int windowWidth, int objectWidth) {
+			return (windowWidth - objectWidth) / 2;
+		}
+
+		/**
+		 * Calculates the centred Y position relative to an object
+		 * 
+		 * @param windowHeight takes in the window height
+		 * @param objectHeight takes in the object height (i.e., building height and
+		 *                     runway height)
+		 * @return returns calculated Y value
+		 */
+		private int taxiWayYPos(int windowHeight, int objectHeight) {
+			// TODO Auto-generated method stub
+			return (windowHeight - objectHeight) / 2;
+		}
+
+		/**
+		 * Rotates the object i.e., runways and taxi ways
+		 * 
+		 * @param g2d      Takes in the graphics component from method
+		 * @param xPos     Takes in the desired x Position
+		 * @param yPos     Takes in the desired y Position
+		 * @param rotation Takes in the desired angle
+		 */
 		public void rotateObject(Graphics2D g2d, int xPos, int yPos, int rotation) {
-			g2d.translate(xPos, yPos);
-			g2d.rotate(Math.toRadians(rotation));
-			g2d.translate(-xPos, -yPos);
+			g2d.translate(xPos, yPos); // Translate the object to yPos and xPos
+			g2d.rotate(Math.toRadians(rotation)); // Rotates the image to desired rotation and converts to radians
+			g2d.translate(-xPos, -yPos); // Translates the object back to the original position
 		}
 
+		/**
+		 * Draws the object and displays it
+		 * 
+		 * @param g2d    Gets the Graphics2D component from method
+		 * @param visual Gets the visual
+		 */
 		public void drawObject(Graphics2D g2d, Rectangle visual) {
-			g2d.draw(visual);
-			g2d.fill(visual);
+			g2d.draw(visual); // Draws the desired visual
+			g2d.fill(visual); // Fills the desired visual
 		}
 
+		/**
+		 * Resets panel so any changes done (i.e., rotating/translating) will be reset
+		 * 
+		 * @param g2d Takes in the Graphics2D component from method
+		 */
 		public void resetTransformation(Graphics2D g2d) {
-			g2d.setTransform(g2d.getDeviceConfiguration().getDefaultTransform());
+			g2d.setTransform(g2d.getDeviceConfiguration().getDefaultTransform()); // Resets window
 		}
 
+		/**
+		 * Does the calculation for centring an object when translating the object
+		 * 
+		 * @param xPos   Takes in the desired X position
+		 * @param yPos   Takes in the desired Y position
+		 * @param width  Takes in the width
+		 * @param height Takes in the height
+		 * @return Returns a list which contains the X and Y position centred
+		 */
 		public int[] centreObject(int xPos, int yPos, int width, int height) {
-			int centreXPos = xPos + (width / 2);
-			int centreYPos = yPos + (height / 2);
+			int centreXPos = xPos + (width / 2); // Calculates X centred
+			int centreYPos = yPos + (height / 2); // Calculates Y centred
 
-			int[] centrePos = { centreXPos, centreYPos };
+			int[] centrePos = { centreXPos, centreYPos }; // Initialises a list which contains X and Y centred
 
-			return centrePos;
+			return centrePos; // Returns the list
 		}
 
 		/**
 		 * draws the planes
 		 * 
-		 * @param g graphics input
 		 * @throws IOException
 		 */
-		public void drawPlanes(Graphics g) throws IOException {
-			Graphics2D g2d = (Graphics2D) g;
+		public void drawPlanes() throws IOException {
 
 			BufferedImage Image;
 
@@ -415,13 +492,11 @@ public class GUIElements extends JFrame {
 		/**
 		 * Draws runways and calculates the position/centres the runways
 		 * 
-		 * @param g        Takes in the graphic object
 		 * @param rotation Takes in variable for rotation
 		 * @param xPos     Takes in the offset X position
 		 * @param yPos     Takes in the offset Y position
 		 */
-		public void drawRunways(Graphics g, int rotation, int xPos, int yPos) {
-			Graphics2D g2d = (Graphics2D) g; // Creates an instance of 2d graphics
+		public void drawRunways(int rotation, int xPos, int yPos) {
 
 			/* Calculates runway X & Y position */
 			runwayXPos = (windowWidth - (RUNWAY_WIDTH)) / 2 + xPos; // Calculates the runway's X position on the X axis
@@ -453,11 +528,8 @@ public class GUIElements extends JFrame {
 
 		/**
 		 * Creates the airport building and calculates the position
-		 * 
-		 * @param g Takes in graphics object
 		 */
-		public void drawAirportBuilding(Graphics g) {
-			Graphics2D g2d = (Graphics2D) g; // Creates an instance of 2d graphics
+		public void drawAirportBuilding() {
 
 			final int offsetY = 100; // Sets the offset of the building
 
@@ -466,12 +538,12 @@ public class GUIElements extends JFrame {
 			buildingYPos = (windowHeight - (BUILDING_HEIGHT)) / 2 + offsetY; // Calculates the buildings Y position on
 																				// the Y axis and offsets by 100
 			int[] getCentredPos = centreObject(buildingXPos, buildingYPos, 350, 115);
-			
+
 			/* Calculates the buildings position when centred */
 			int centreXBuilding = getCentredPos[0]; // Calculates the building centre. (350 is what got to the
-															// position I wanted)
+													// position I wanted)
 			int centreYBuilding = getCentredPos[1]; // Calculates the building centre. (115 is what got the the
-															// position on I wanted)
+													// position on I wanted)
 
 			g2d.setColor(buildingColour); // Sets the building colour to light gray
 
@@ -496,23 +568,20 @@ public class GUIElements extends JFrame {
 
 		/**
 		 * Creates the gates and calculates the positions
-		 * 
-		 * @param g
 		 */
-		public void drawGates(Graphics g) {
-			Graphics2D g2d = (Graphics2D) g; // Creates an instance of 2d graphics
+		public void drawGates() {
 
 			final int gateOffsetY = -44; // Offsets the gate on the Y axis
 			final int gateOffsetX = 2; // Offsets the gate on the X axis
 			final int gateSpacing = 85; // Determines the spacing between each gate
-			
+
 			int[] getCentredPos = centreObject(buildingXPos, buildingYPos, BUILDING_WIDTH, BUILDING_HEIGHT);
-			
+
 			/* Calculates the Gates centre */
 			int centreXBuilding = getCentredPos[0]; // Calculates the buildings X position on the X
-																		// axis
+													// axis
 			int centreYBuilding = getCentredPos[1]; // Calculates the buildings Y position on the Y
-																		// axis
+													// axis
 
 			rotateObject(g2d, centreXBuilding, centreYBuilding, BUILDING_ROTATION);
 
@@ -533,15 +602,18 @@ public class GUIElements extends JFrame {
 		}
 
 		/**
-		 * Draws and calculates the taxiway's position
+		 * Creates the taxi ways with the given information
 		 * 
-		 * @param g
+		 * @param xPos          Takes in the desired X position
+		 * @param yPos          Takes in the desired Y position
+		 * @param width         Takes in the desired width
+		 * @param height        Takes in the desired height
+		 * @param rotationAngle Takes in in the angle for rotating
 		 */
-		public void drawTaxiWays(Graphics g, int xPos, int yPos, int width, int height, int rotationAngle) {
-			Graphics2D g2d = (Graphics2D) g;
-			
+		public void drawTaxiWays(int xPos, int yPos, int width, int height, int rotationAngle) {
+
 			int[] getCentredPos = centreObject(xPos, yPos, width, height);
-			
+
 			int centreX = getCentredPos[0];
 			int centreY = getCentredPos[1];
 
