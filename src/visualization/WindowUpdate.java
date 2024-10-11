e129fc11706725779b018ed9b5958fc29eaccb93
Lim Ping <janthonyborja@gmail.com>
2024-10-11 07:56:39 -0500

    Added comments and properly formatted code

--- src/visualization/WindowUpdate.java
+++ src/visualization/WindowUpdate.java
@@ -8,55 +8,43 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class WindowUpdate extends JFrame{
+public class WindowUpdate extends JFrame {
 	private static final long serialVersionUID = 1435203151832523191L;
-	
+
 	/**
 	 * Updates the screen constantly
 	 * 
-	 * @param Panel Takes in the Panel for updating what's on the screen
+	 * @param mainPanel Takes in the Panel for updating what's on the screen
+	 * @param tower     Takes in tower class for creating planes
 	 */
-	public WindowUpdate(JPanel Panel, Tower tower) {
-		Timer timer = new Timer(Main.UPDATE_INTERAVAL,  new ActionListener() {
-		//PlaneAttributes one = new PlaneAttributes();
-		PlaneBehavior two = new PlaneBehavior();
-		int vel;
-		double z;
-		boolean takeoff, landing;
-		
+	public WindowUpdate(JPanel mainPanel, Tower tower) {
+		Timer timer = new Timer(Main.UPDATE_INTERAVAL, new ActionListener() {
+			// PlaneAttributes one = new PlaneAttributes();
+			PlaneBehavior two = new PlaneBehavior();
+			int vel;
+			double z;
+			boolean takeoff, landing;
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				
-				for(PlaneAttributes plane: tower.getPlanes()) {
+
+				for (PlaneAttributes plane : tower.getPlanes()) {
 //					plane.move(-5, -2.9);
 					plane.move(2.5, -1.45);
 					plane.turn(2);
 				}
-				Panel.repaint();
-				
-				
-				
-				
-				
-				
-				
+				mainPanel.repaint();
+
 				/*
-				one.takeoff(takeoff);
-				one.landing(landing);
-				
-				this.takeoff = takeoff;
-				this.landing = landing;
-				
-				
-				if(takeoff == true) {
-					one.upVelocity(vel, takeoff);
-					one.upAlt(z);
-				}else if(landing == true)
-					one.downVelocity(vel, landing);
-					one.downAlt(z);
-				*/
-				
+				 * one.takeoff(takeoff); one.landing(landing);
+				 * 
+				 * this.takeoff = takeoff; this.landing = landing;
+				 * 
+				 * 
+				 * if(takeoff == true) { one.upVelocity(vel, takeoff); one.upAlt(z); }else
+				 * if(landing == true) one.downVelocity(vel, landing); one.downAlt(z);
+				 */
+
 			}
 		});
 		timer.start();
